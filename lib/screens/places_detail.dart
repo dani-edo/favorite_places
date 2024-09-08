c712edbfe8c5b1c06d805601c476c8a35bd8c4a0
dani_edo <dani.edogawa.17@gmail.com>
2024-09-08 13:41:47 +0700

    feat: preview image on list and detail

--- lib/screens/places_detail.dart
+++ lib/screens/places_detail.dart
@@ -15,13 +15,15 @@ class PlacesDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(place.title),
       ),
-      body: Center(
-        child: Text(
-          place.title,
-          style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-        ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ],
       ),
     );
   }
