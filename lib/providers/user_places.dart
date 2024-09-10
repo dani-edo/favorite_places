7e11966140fa9ad0ea8d081fbc4121d3d82ac874
dani_edo <dani.edogawa.17@gmail.com>
2024-09-10 22:44:08 +0700

    feat: save image file to filesystem

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,14 +2,20 @@ import 'dart:io';
 
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String place, File file, PlaceLocation location) {
+  void addPlace(String place, File file, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(file.path);
+    final copiedImage = await file.copy('${appDir.path}/$filename');
+
     final newPlace = Place(
       title: place,
-      image: file,
+      image: copiedImage,
       location: location,
     );
     state = [newPlace, ...state];
